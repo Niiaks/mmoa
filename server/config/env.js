bcfddbc5a8f129419517e2f64fe37ff52bebdbc0
Niiaks <napappoe007@st.ug.edu.gh>
2026-04-10 13:53:17 +0000

    add all necessary env variables

--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,7 @@
 export const ENV = {
-    dbUrl: process.env.DB_URL
-
-}
+  dbUrl: process.env.DB_URL,
+  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT,
+  paystackSecretKey: process.env.PAYSTACK_SECRET_KEY,
+  clientUrl: process.env.CLIENT_URL,
+};
