376d9fe5841324c19ef5cbf0ce030eb0df071bc4
Niiaks <napappoe007@st.ug.edu.gh>
2026-04-11 18:46:18 +0000

    add cookie parser for cookies

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 import "dotenv/config";
 import express from "express";
 import { connectDB, disconnectDB } from "./config/db.js";
-
+import { contributionRouter } from "./routes/contribution.route.js";
+import { authRouter } from "./routes/auth.route.js";
+import { webhookRouter } from "./routes/webhook.route.js";
+import { campaignRouter } from "./routes/campaign.route.js";
+import cookieParser from "cookie-parser";
+import { ENV } from "./config/env.js";
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use("/api/v1/webhook", webhookRouter);
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/contributions", contributionRouter);
+app.use("/api/v1/campaigns", campaignRouter);
 
 let server;
 
 const startServer = async () => {
   try {
     await connectDB();
-    server = app.listen(port, () => {
-      console.log(`server is running on http://localhost:${port}`);
+    server = app.listen(ENV.port, () => {
+      console.log(`server is running on http://localhost:${ENV.port}`);
     });
   } catch (error) {
     console.error("something in starting server broke:", error);
