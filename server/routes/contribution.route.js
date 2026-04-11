a76b05f9920e853477a0c6dc63c594b454437d59
Niiaks <napappoe007@st.ug.edu.gh>
2026-04-11 20:51:34 +0000

    setup routes for contributions

--- server/routes/contribution.route.js
+++ server/routes/contribution.route.js
@@ -1,3 +1,13 @@
 import express from "express";
+import {
+  getContributions,
+  initializePayment,
+  verifyPayment,
+} from "../controllers/contribution.controller.js";
+import { protectedRoute } from "../middleware/auth.js";
 
 export const contributionRouter = express.Router();
+
+contributionRouter.post("/", initializePayment);
+contributionRouter.get("/verify", verifyPayment);
+contributionRouter.get("/:campaignId", protectedRoute, getContributions);
