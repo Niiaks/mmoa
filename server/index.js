52ec2b829837d919a42df57ca543de02b5a2835c
Niiaks <napappoe007@st.ug.edu.gh>
2026-04-22 12:20:57 +0000

    fix: simplify CORS origin configuration by removing redundant URL

--- server/index.js
+++ server/index.js
@@ -21,10 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      ENV.clientUrl,
-      "https://ingredients-month-cotton-certainly.trycloudflare.com",
-    ],
+    origin: ENV.clientUrl,
     credentials: true,
   }),
 );
