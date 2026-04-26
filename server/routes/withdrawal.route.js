fb4de567040fa11eeeece764b2c90f74925439a5
Niiaks <napappoe007@st.ug.edu.gh>
2026-04-26 16:55:35 +0000

    feat: apply withdrawalLimiter middleware to withdrawal routes

--- server/routes/withdrawal.route.js
+++ server/routes/withdrawal.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.js";
-import { withdrawMoney, previewWithdrawal } from "../controllers/withdrawal.controller.js";
+import {
+  withdrawMoney,
+  previewWithdrawal,
+} from "../controllers/withdrawal.controller.js";
 import { withdrawalLimiter } from "../middleware/rateLimiter.js";
 
 export const withdrawalRouter = express.Router();
 
 withdrawalRouter.use(protectedRoute);
-withdrawalRouter.use(withdrawalLimiter);
 
-withdrawalRouter.post("/:id", withdrawMoney);
+withdrawalRouter.post("/:id", withdrawalLimiter, withdrawMoney);
 withdrawalRouter.get("/preview/:id", previewWithdrawal);
